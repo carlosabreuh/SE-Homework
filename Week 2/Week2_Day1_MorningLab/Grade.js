1afeadd0f3b57ca6e19ef8c3b4d8fa6e901dbd3a
carlosabreuh <carlos@abreuh.com>
2020-11-20 14:07:36 -0500

    Week 2

---
+++ Week 2/Week2_Day1_MorningLab/Grade.js
@@ -0,0 +1,28 @@
+
+//Tell Me your grade
+
+console.log("grade");
+
+var grade = 100;
+
+if (grade>=90){
+    console.log('A')}
+else if (grade>=80){
+    console.log('B')}
+else if (grade>=70){
+    console.log('C')}
+else if (grade>=55){
+    console.log('D')}
+else{ (grade>=55)
+    console.log('F')
+}
+
+
+//
+
+
+
+
+
+
+ 
