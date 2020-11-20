e2d8391b4f8ec9f0ca57653de485a9ce480b59d1
carlosabreuh <carlos@abreuh.com>
2020-11-20 15:59:35 -0500

    Week 2

---
+++ Week 2/Week2_Day1_MorningLab/Loops.js
@@ -0,0 +1,12 @@
+// var i = -1;
+// console.log(i);
+
+// for (let i =0; i<10 ; i++){
+    let i = 0;
+    do {   // do-while-loop-- will continue until it is
+    console.log(i);
+        i++;
+} while (i<10);
+
+
+// console.log(i);
