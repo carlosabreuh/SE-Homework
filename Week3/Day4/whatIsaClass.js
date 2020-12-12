14db392a342a5dcfbe19d285c067077ee7cd4d5e
carlosabreuh <carlos@abreuh.com>
2020-12-12 11:27:57 -0500

    Create whatIsaClass.js

---
+++ Week3/Day4/whatIsaClass.js
@@ -0,0 +1,26 @@
+let hotelCalifornia = {
+  name: "Hotel California",
+  location: "California",
+  rating: 4,
+  vacancies: true,
+  tags: ["pink champagne", "wine", "lovely", "can't leave"],
+  rooms: [
+    {
+      roomNumber: 102,
+      size: "Queen Double",
+      price: 55,
+      booked: true,
+    },
+    {
+      roomNumber: 202,
+      size: "King Suite",
+      price: 68,
+      booked: false,
+    },
+    {
+      roomNumber: 404,
+      size: "Queen Double",
+      booked: false,
+    },
+  ],
+};
