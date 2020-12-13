7d6e28c05eff99d3eac891d1ca36be7038f91109
carlosabreuh <carlos@abreuh.com>
2020-12-13 16:27:25 -0500

    Update Week3_Day4_AfternoonLab.js

--- Week3/Day4/Week3_Day4_AfternoonLab.js
+++ Week3/Day4/Week3_Day4_AfternoonLab.js
@@ -4,41 +4,37 @@ Create two objects of this class - both with a unique superPower
 create three methods for this class - include one method named userSuperPower()
 Create two objects for this class */
 
-class Aminal {
-    constructor(name, age, color, legs, superPower){
-    this.name = name;
-    this.age = age;
-    this.color = color;
-    this.legs = legs;
-    this.superPower = superPower;
-    }
-    
-    // Three methods
-    // superPower(){
-
-    // }
-    chew(){
-        console.log("I chew everything!!");
-    }
-    play(){
-        console.log("I play all the time!!");
-    }
-    run(){
-        console.log("I run in the park!!");
+class Animal{//CLASS
+    constructor(name, age, color, legs, superPower){//is a function
+        this.name = name;
+        this.age = age;
+        this.color = color;//must add another property to method chain
+        this.legs = legs;
+        this.superPower = superPower;
+    }userSuperPower(){
+    }speaks(){
+    }run(){
     }
 }
-
-// Two objects
-
-var obj1 = new Animal('Chloe','6months','Brown',4,'Chews everyting');
-var obj2 = new Animal('kitto','6years','White',4,'Mail Destroyer');
-
-
-class User{ 
+let dog = new Animal('Charlie', 16, 'red-brown' , 4, 'reads your mind');
+let puppy = new Animal('Darcy', 1, 'black and white', 4, 'makes you happy');
+// Create a class name User
+// This class with have firstName, lastName, department attributes 
+// a method name changeDepartment()
+// Create three instances of this class
+// Change department for two of the instances
+// Create three objects for this class
+class User{
     constructor(firstName, lastName, department){
-        this.firstName = firstName
-        this.lastName = lastName
-        this.department = department
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+    }changeDepartment(){
+        console.log("FD");
     }
-    
 }
+let user1 = new User('Sara', 'Peña', 'SE');//instance of a class
+//object  = instance of a class
+let user2 = new User('Eric', 'Parker', 'FD');
+let user3 = new User('Alex','Walker', 'PD');
+console.log(user3.changeDepartment());
