f1830b84279284b5a75c2bad92859694c9c2bff2
carlosabreuh <carlos@abreuh.com>
2020-12-13 16:27:30 -0500

    Update Week3_Day4_MorningLab.js

--- Week3/Day4/Week3_Day4_MorningLab.js
+++ Week3/Day4/Week3_Day4_MorningLab.js
@@ -6,21 +6,33 @@
 // Find the difference between abstract class and an interface
 // Which of the two can have implementation of a method in the class? 
 
+// class Fish {
+//   constructor() {}
+//   swim() {
+//     console.log("I can Swim");
+//   }
+// }
+
+// class Nemo extends Fish {
+//   constructor(color) {
+//     this.color = color;
+//   }
+// }
+
+// let Nemo1 = new Nemo ("purple");
+// console.log(Nemo1.swim());
+
 class Fish {
   constructor() {}
   swim() {
     console.log("I can swim");
   }
 }
-
 class Nemo extends Fish {
   constructor(color) {
     super();
     this.color = color;
   }
 }
-
 var Nemoo = new Nemo("purple");
 Nemoo.swim();
-
-
