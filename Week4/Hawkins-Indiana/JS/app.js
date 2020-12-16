cf891a998d59368da3d2158a3630771a3a8bdaf9
carlosabreuh <carlos@abreuh.com>
2020-12-16 15:59:02 -0500

    Create app.js

---
+++ Week4/Hawkins-Indiana/JS/app.js
@@ -0,0 +1,9 @@
+console.log("Stanger things are coming!!!");
+$(() => () {}
+const addH2 = () => {
+    let $h2 = $('<h2>').text("Just getting started");
+    $('body').append($h2);
+}
+
+
+// addH2();
