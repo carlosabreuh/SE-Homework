6c5319e3308fc7ebcd8f1779b69e0bcf3078a74c
carlosabreuh <carlos@abreuh.com>
2020-12-16 21:56:17 -0500

    personStore.introduce

---
+++ Week4/CSX/Introduce.js
@@ -0,0 +1,20 @@
+const personStore = {
+    greet: function() {
+      console.log('hello');
+    }
+  }
+  
+  function personFromPersonStore(name, age) {
+      const person = Object.create(personStore);
+    person.name = name;
+    person.age = age;
+    return person;
+  }
+  
+  const sandra = personFromPersonStore('Sandra', 26);
+  
+  // add code here
+  personStore.introduce = function() {console.log(`Hi, my name is ${this.name}`)};
+  
+  // Uncomment this line to check your work!
+  sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
