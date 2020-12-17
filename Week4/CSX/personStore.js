a6e636a2b6958912875f07bd5c57e4e5d2e729b2
carlosabreuh <carlos@abreuh.com>
2020-12-16 21:21:29 -0500

    Create personStore.js

---
+++ Week4/CSX/personStore.js
@@ -0,0 +1,12 @@
+// Inside personStore object, create a property greet where the value is a function that logs "hello".
+
+const personStore = {
+	// add code here
+greet: function() {
+  console.log("Hello");
+}
+
+};
+
+// Uncomment this line to check your work!
+personStore.greet(); // -> Logs 'hello'
