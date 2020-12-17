37d031a4e05fab768e3d638f1e4d8ef3ba5c8129
carlosabreuh <carlos@abreuh.com>
2020-12-16 21:02:15 -0500

    CSX Exercises - MakePerson

---
+++ Week4/CSX/makePerson.js
@@ -0,0 +1,22 @@
+// Create a function that accepts two inputs (name and age) and returns an object. Let's call this function makePerson. This function will:
+// Create an empty object
+// Add a name property to the newly created object with its value being the name argument passed into the function
+// Add an age property to the newly created object with its value being the "age" argument passed into the function
+// Return the object
+
+
+
+function makePerson(name, age) {
+	// add code here
+return {
+  name: name,
+  age: age,
+	}
+}
+
+const vicky = makePerson('Vicky', 24);
+
+
+// Uncomment these lines to check your work!
+ console.log(vicky.name); // -> Logs 'Vicky'
+console.log(vicky.age); // -> Logs 24
