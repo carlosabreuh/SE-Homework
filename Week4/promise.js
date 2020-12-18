3b4f86787e7545849a6e1aa73bd5b7511a428ac5
carlosabreuh <carlos@abreuh.com>
2020-12-18 15:57:56 -0500

    Create promise.js

---
+++ Week4/promise.js
@@ -0,0 +1,20 @@
+var num = 0;
+
+let prom = new Promise((resolve, reject) => {
+    let num = Math.random();
+    if (num < .5 ){
+      resolve('Yay!');
+    } else {
+      reject('Ohhh noooo!');
+    }
+  });
+   
+  const handleSuccess = (resolvedValue) => {
+    console.log(resolvedValue);
+  };
+   
+  const handleFailure = (rejectionReason) => {
+    console.log(rejectionReason);
+  };
+   
+  prom.then(handleSuccess, handleFailure);
