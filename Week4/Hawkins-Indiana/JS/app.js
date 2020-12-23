25c43e449eb52c7f1f707ff9e7cbb9e83379863d
carlosabreuh <carlos@abreuh.com>
2020-12-22 22:39:24 -0500

    Update app.js

--- Week4/Hawkins-Indiana/JS/app.js
+++ Week4/Hawkins-Indiana/JS/app.js
@@ -1,9 +1,48 @@
-console.log("Stanger things are coming!!!");
-$(() => () {}
-const addH2 = () => {
-    let $h2 = $('<h2>').text("Just getting started");
-    $('body').append($h2);
-}
+// $('#submit-btn').on('click', () => {
+//     console.log('$(#input-box').val();
+// });
 
 
-// addH2();
+$('form').on('submit', (event) => { 
+    console.log($('#input-box').val());
+    event.preventDefault();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // console.log("Stanger things are coming!!!");
+// // $(() => () {}
+// // const addH2 = () => {
+// //     let $h2 = $('<h2>').text("Just getting started");
+// //     $('body').append($h2);
+// // }
+
+
+// // addH2();
+
+// $.ajax({
+//     url: 'http://www.omdbapi.com/?apikey'
+// })
