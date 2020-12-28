a09af282418bcef7504bae23d51c5a23ff98e1d0
carlosabreuh <carlos@abreuh.com>
2020-12-27 21:50:29 -0500

    Update app.js

--- Week4/complaints_app/JS/app.js
+++ Week4/complaints_app/JS/app.js
@@ -2,8 +2,9 @@ let key = '53aa2cd6'
 let movieTitle = 'Mad Max: Fury Road'
 
 $(() => {
-   $('form').on('submit', (event) => {
-       event.preventDefault()
+   $("button").click( (e) => {
+       $("tbody#callData").empty();
+       e.preventDefault()
        const userInput = $('input[type="text"]').val()
        const promise = $.ajax({
         url: "https://data.cityofnewyork.us/resource/erm2-nwe9.json"
