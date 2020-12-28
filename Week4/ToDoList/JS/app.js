32cd19da75589271019c09ce1c692eaae70046dc
carlosabreuh <carlos@abreuh.com>
2020-12-28 16:43:27 -0500

    Pizza Rat

--- Week4/ToDoList/JS/app.js
+++ Week4/ToDoList/JS/app.js
@@ -1,28 +1,50 @@
-const toDoList = [];
-
-
-const completeButton = "<br> <button type = 'submit'> COMPLETE </button>";
-const removeButton = "<br> <button type = 'submit'> REMOVE </button>";	
-
-
-$('form').on('submit', (event) => {
-  console.log( $('#input-box').val() );
-
-  toDoList.push( $('#input-box').val() );
-
-  event.preventDefault();
-  $(event.currentTarget).trigger('reset');
-
-  render("#toDoUL",toDoList);
-});
-
-
-
-const render = (listID,arrList)=>{
-	arrLength = arrList.length - 1;
-	console.log(arrLength);
-    $(listID).append(`<li id = ${arrLength}> ${arrList[arrLength]} ${completeButton} </li>`);
-    $('li').on('click', (event) =>{
-    	$(event.currentTarget).css('text-decoration', 'line-through');
+//Window onload
+$(() => {
+  //Listener for add button below Things to Do.
+  $('form').on('submit', function (e) { // e for Empty form
+    //Prevented the form from submitting
+    e.preventDefault();  // Stops the object from dissapearing after the input text is entered. 
+    //Created a list to store the todo items
+    let $createdList = $(`
+                <li>
+                <p>${$('#input-box').val()}</p>
+                
+                </li>
+            `);
+    // Makes the "task to complete"  #8ac926 
+    $createdList.css('background-color', '#8ac926');
+    //Reset the input box to clear the content so you can add more todo items
+    $(e.currentTarget).trigger('reset');
+    //Add edthe remove button to the todo list item
+    let remove = $(Remove()).click(
+      //Anonymous function to allow you to target the parent of the input which is the the current todo list item
+      function () {
+        let list = $(this).parent();
+        list.fadeOut(function () {
+          list.remove();
+        });
+      }
+    );
+    //Added an input button to remove the completed task and add the pink style
+    let complete = $(
+      "<input type='submit' value='COMPLETED' id='completed'class='btn btn-secondary'/>"
+    ).click(function () {
+      var list2 = $(this).parent();
+      list2.fadeOut(function () {
+        $createdList.css('background-color', '#3d405b');
+        $('#complete').append(list2);
+        $createdList.append(remove);
+        list2.fadeIn();
+      });
+      $(this).remove();
     });
+    //Added the completed list item to the list
+    $createdList.append(complete);
+    //Added the created list item to the todo list
+    $('#todo').append($createdList);
+  });
+});
+function Remove() {
+  return "<input type='submit' value='REMOVE' id='remove'class='btn btn-secondary'/>";
 }
+
--- Week4/complaints_app/JS/app.js
+++ Week4/complaints_app/JS/app.js
@@ -1,25 +1,43 @@
-let key = '53aa2cd6'
-let movieTitle = 'Mad Max: Fury Road'
-
 $(() => {
-   $("button").click( (e) => {
-       $("tbody#callData").empty();
-       e.preventDefault()
-       const userInput = $('input[type="text"]').val()
-       const promise = $.ajax({
-        url: "https://data.cityofnewyork.us/resource/erm2-nwe9.json"
-       })
-       
-       promise.then(
-           (data) => {
-               console.log(data)
-               $('#title').html(data.Title)
-               $('#year').html(data.Year)
-               $('#rated').html(data.Rated)
-           },
-           () => {
-               console.log('bad request')
-           }
-       )
-   })
-})
+  $("button").on('click', function (event) {
+          event.preventDefault();
+          console.log(event.target);
+
+          const userInput = event.target.innerHTML.toUpperCase();
+          let numberOfRequest = $("input")[0].value;
+          if (!(numberOfRequest)) {
+              numberOfRequest = 10;
+          }
+
+          $.ajax({
+              url: "https://data.cityofnewyork.us/resource/erm2-nwe9.json",
+              type: "GET",
+              data: {
+                  '$limit': numberOfRequest,
+                  agency: "NYPD",
+                  borough: userInput,
+              },
+          }).then(
+              function (data) {
+                  console.log(data);
+                  for (let i = 0; i < data.length; i++) {
+                      let entry = $("<p>");
+                      entry.text(`${data[i].descriptor}`);
+                      let button = $("<button>");
+                      button.text("Police Response");
+                      let response = $("<span>");
+                      response.text(`${data[i].resolution_description}`).hide();
+                      button.click(() => {
+                          response.toggle();
+                      });
+                      entry.append(button);
+                      entry.append(response);
+                      $("#container").append(entry);
+                  }
+              },
+              function () {
+                  console.log("bad request");
+              }
+          );
+      });
+});
