7e0988b84b913890e0d3684aecdca6b748375e4b
carlosabreuh <carlos@abreuh.com>
2021-01-13 19:40:02 -0500

    Tardis

---
+++ MOD-2/Week-1/DAY3/w1d3-lab/tardis/app.js
@@ -0,0 +1,63 @@
+class App extends React.Component {
+   tardis = {
+  name: 'Time and Relative Dimension in Space',
+  caps: false,
+}
+changeIt = (text) => {
+  if (this.state.tardis.caps) {
+    this.setState({
+      tardis: {
+        name: text.toLowerCase(),
+        caps: false
+      }
+    })
+  } else {
+    this.setState({
+      tardis: {
+        name: text.toUpperCase(),
+        caps: true
+      }
+    })
+  }
+}
+  render() {
+      return(
+          <div>
+             <DivOne />
+          </div>
+      )
+  }
+}
+class DivOne extends React.Component {
+    render () {
+        return(
+            <div>
+                <DivTwo />
+            </div>
+        )
+    }
+}
+class DivTwo extends React.Component {
+    render () {
+        return(
+            <div>
+               <DivThree />
+            </div>
+        )
+    }
+}
+class DivThree extends React.Component {
+    render () {
+        return(
+        <div onClick={this.changeIt}>
+            <div>
+             <h3>Futbol is Life!! "Davi Sanchez"</h3>
+            </div>
+             <div>
+             <h3>FUTBOL IS LIFE!! "DAVI SANCHEZ"</h3>
+            </div>
+        </div>
+        )
+    }
+}
+ReactDOM.render(<App />, document.querySelector(".container"))
