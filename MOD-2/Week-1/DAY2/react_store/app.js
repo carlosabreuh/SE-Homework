2c6e1c7dfccf26367df28e8184e74acd8a7255ae
carlosabreuh <carlos@abreuh.com>
2021-01-14 20:54:55 -0500

    Update app.js

--- MOD-2/Week-1/DAY2/react_store/app.js
+++ MOD-2/Week-1/DAY2/react_store/app.js
@@ -104,7 +104,5 @@ class App extends React.Component {
 //   }
 // }
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('.container')
+ReactDOM.render(<App />, document.querySelector('.container')
 )
