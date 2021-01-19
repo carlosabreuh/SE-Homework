7fccb57a36dda5e5615889078db1fa6ef5e3a4b2
carlosabreuh <carlos@abreuh.com>
2021-01-18 21:40:40 -0500

    Initial Commit

---
+++ MOD-2/Week-1/DAY2/app.js
@@ -0,0 +1,25 @@
+// $('#submit-btn').on('click', () => {
+//     console.log('$(#input-box').val();
+// });
+
+
+$('form').on('submit', (event) => { 
+    console.log($('#input-box').val());
+    event.preventDefault();
+});
+
+
+
+// console.log("Stanger things are coming!!!");
+// $(() => () {}
+// const addH2 = () => {
+//     let $h2 = $('<h2>').text("Just getting started");
+//     $('body').append($h2);
+// }
+
+
+addH2();
+
+$.ajax({
+    url: 'http://www.omdbapi.com/?apikey'
+})
